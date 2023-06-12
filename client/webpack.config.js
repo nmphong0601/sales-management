6cec1488d3ad852871778807db1b4d863c787028
Nguyen Minh Phong <nmphong0601@gmail.com>
2023-06-12 13:09:19 +0700

    commit - update HMR

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,52 +6,52 @@ const InterpolateHtmlPlugin = require("interpolate-html-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-  entry: ["webpack/hot/dev-server", "./src/index.js"],
+  entry: ['webpack/hot/dev-server', './src/index.js'],
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "[name].[contenthash].js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
   },
   resolve: {
     alias: {
-      containers: path.resolve(__dirname, "src/containers/"),
-      components: path.resolve(__dirname, "src/components/"),
-      common: path.resolve(__dirname, "src/common/"),
-      pages: path.resolve(__dirname, "src/pages/"),
-      services: path.resolve(__dirname, "src/services/"),
-      process: "process/browser",
+      containers: path.resolve(__dirname, 'src/containers/'),
+      components: path.resolve(__dirname, 'src/components/'),
+      common: path.resolve(__dirname, 'src/common/'),
+      pages: path.resolve(__dirname, 'src/pages/'),
+      services: path.resolve(__dirname, 'src/services/'),
+      process: 'process/browser',
     },
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-          "postcss-loader",
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
         ],
       },
       {
         test: /\.png$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              mimetype: "image/png",
+              mimetype: 'image/png',
             },
           },
         ],
       },
       {
         test: /\.svg$/,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
@@ -59,16 +59,16 @@ const config = {
     hot: true,
     compress: true,
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, 'public'),
     },
     historyApiFallback: true, // Allow refreshing of the page
     server: {
-      type: "https",
+      type: 'https',
       options: {
-        key: fs.readFileSync("cert.key"),
-        cert: fs.readFileSync("cert.crt"),
-        ca: fs.readFileSync("ca.crt"),
-        passphrase: "webpack-dev-server",
+        key: fs.readFileSync('cert.key'),
+        cert: fs.readFileSync('cert.crt'),
+        ca: fs.readFileSync('ca.crt'),
+        passphrase: 'webpack-dev-server',
       },
     },
   },
@@ -81,22 +81,23 @@ const config = {
     //   filename: "index.html",
     // }),
     new HtmlWebpackPlugin({
-      title: "FE App",
-      template: path.resolve(__dirname, "public", "index.html"),
-      filename: "index.html",
+      title: 'FE App',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
     }),
-    new InterpolateHtmlPlugin({ PUBLIC_URL: "/public" }),
+    new InterpolateHtmlPlugin({ PUBLIC_URL: '/public' }),
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
     new MiniCssExtractPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
+          name: 'vendors',
+          chunks: 'all',
         },
       },
     },
